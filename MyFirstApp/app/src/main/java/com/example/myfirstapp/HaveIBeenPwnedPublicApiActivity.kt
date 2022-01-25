521d3f8ca522409d8e6172f034cec4781fc579ce
FeeZotter <90189171+FeeZotter@users.noreply.github.com>
2022-01-25 15:36:03 +0100

    added funktionality to HIBP button

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/HaveIBeenPwnedPublicApiActivity.kt
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/HaveIBeenPwnedPublicApiActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myfirstapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,10 +9,7 @@ import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import org.json.JSONObject
 import org.json.JSONException
-
-import com.android.volley.Response
-
-
+import com.android.volley.toolbox.Volley
 
 
 class HaveIBeenPwnedPublicApiActivity : AppCompatActivity() {
@@ -24,35 +22,27 @@ class HaveIBeenPwnedPublicApiActivity : AppCompatActivity() {
             test.put("name", "NAME OF STUDENT")
             test.put("year", "3rd")
             test.put("curriculum", "Arts")
-            test.put("birthday", "5/5/1993")
+            test.put("birthday", "5.5.1993")
         } catch (e: JSONException) {
-            // TODO Auto-generated catch block
             e.printStackTrace()
         }
 
         findViewById<TextView>(R.id.editTextTextMultiLine).apply { text = test.toString() }
     }
-  //  findViewById<TextView>(R.id.editTextTextMultiLine)
+
+    @SuppressLint("SetTextI18n")
     fun test(view: View)
     {
-        val test2 = JSONObject()
-        var textView = findViewById<TextView>(R.id.editTextTextMultiLine)
-
-
-/*
-        val test = JSONObject()
-        try {
-            test.put("id", (0..10).random())
-            test.put("name", "NAME OF STUDENT")
-            test.put("year", "3rd")
-            test.put("curriculum", "Arts")
-            test.put("birthday", (0..10).random().toString()+"/"+(0..10).random().toString()+"/1993")
-        } catch (e: JSONException) {
-            // TODO Auto-generated catch block
-            e.printStackTrace()
-        }
-
-        findViewById<TextView>(R.id.editTextTextMultiLine).apply { text = test.toString() }
-*/
+        val textView = findViewById<TextView>(R.id.editTextTextMultiLine)
+        val url = "https://haveibeenpwned.com/api/v3/breach/" + findViewById<TextView>(R.id.breachText).text
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            { response ->
+                textView.text = "Response: %s".format(response.toString())
+            },
+            { error ->
+                textView.text = error.toString()
+            }
+        )
+        Volley.newRequestQueue(this).add(jsonObjectRequest)
     }
 }
